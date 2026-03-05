33a732de108dfb3e1bed96399c3fbbc9595196d6
Kayla Thornton <kaylathornton55@icloud.com>
2026-03-04 20:16:17 -0500

    update: cards table initialized

--- lib/data/card_database.dart
+++ lib/data/card_database.dart
@@ -1,18 +1,21 @@
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
+import '../data/folder_database.dart';
 
-// class handles SQL logic associated with Folder database
-class Folder{
+// class handles SQL logic associated with Card database
+class CardDatabase{
   // database related information
   static const dbPath = 'Database.db';
-  static const table = 'Folders';
+  static const table = 'Cards';
   static const version = 1;
 
   // column names to be references when performing database operations
   static const columnId = 'id';
-  static const columnFolderName = 'folder_name';
-  static const columnTimestamp = 'timestamp';
+  static const columnCardName = 'card_name';
+  static const columnSuit = 'suit';
+  static const columnImage = 'image_url';
+  static const columnFolderId = 'folder_id';
 
   late Database _data;
 
@@ -26,8 +29,12 @@ class Folder{
   Future _onCreate(Database data, int version) async {
     return await data.execute('''CREATE TABLE $table(
       $columnId AUTO_INCREMENT PRIMARY KEY,
-      $columnFolderName TEXT,
-      $columnTimestamp TIMESTAMP
+      $columnCardName TEXT,
+      $columnSuit TEXT,
+      $columnImage TEXT,
+      CONSTRAINT fk_CardFolder 
+        FOREIGN KEY ($columnId) 
+        REFERENCES Folders(id)
     )''');
   }
 }
