b1aeb102b708170bd296ac011ea267f956fd03d5
ZdotOrg <shieldsthefirst@gmail.com>
2026-03-05 06:05:59 -0500

    chore: add imports and screen structure

---
+++ lib/screens/card_screen.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import '../model/folder_model.dart';
+import '../model/card_model.dart';
+import '../repositories/card_repository.dart';
+import 'modify_card_screen.dart';
+import '../screens/delete_confirmation.dart';
