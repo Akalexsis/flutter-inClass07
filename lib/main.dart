7d8a012c2089d4f6e059e3d3ce6f5e1b17885d3e
ZdotOrg <shieldsthefirst@gmail.com>
2026-03-05 20:12:59 -0500

    chore: fix imports

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const FoldersScreen(),//folder screen as home screen
+      home: const FoldersScreen(),//folder screen as home
     );
   }
 }
